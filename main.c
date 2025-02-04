e7c43bcf8d5a144cfecdf2a14b2a9e9c118ab7c9
Caleb Monti <76889116+WhelpImAGhost@users.noreply.github.com>
2025-02-03 21:51:07 -0800

    File and mode arg input

--- main.c
+++ main.c
@@ -6,27 +6,27 @@
 
  */
 
+// 
+
 
 int main(int argc, char *argv[]){
-       // Set default filename
-    char *default_filename = "Other_dins/Default.din";
+
+    // Local variables
+    int mode = 0;
+
+    // Set default filename
+    char *default_filename = "Test Memory Files/prog.mem";
     char *filename = default_filename;
 
-    // Cache usage and tracking variables
-    int cache_size = CACHE_SIZE;
-    int tag, set_index, byte_select;
-    int CacheResult;
-    int SnoopReply;
+
 
 
     // Flags for setting non-default variable vvalues
     for( argc--, argv++; argc > 0; argc-=2, argv+=2 ) {
-        if (strcmp(argv[0], "-m" ) == 0 ) 
-            mode = atoi(argv[1]); // Set normal operation
-        else if (strcmp(argv[0], "-f" ) == 0 ) 
+        if (strcmp(argv[0], "-f" ) == 0 ) 
             filename = argv[1]; // Set input file
-        else if (strcmp(argv[0], "-c") == 0)
-            cache_size = atoi(argv[1]);
+        else if (strcmp(argv[0], "-m" ) == 0 ) 
+            mode = atoi(argv[1]); // Set normal operation
         else { 
             printf("\nInvalid Arguments\n"); exit(-1); 
         }
